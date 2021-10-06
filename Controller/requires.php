ba1d2b7847079c28e2e3fc25a5cec167dc1dce0e
Shyhyro <Bosquet.laurie@outlook.com>
2021-10-06 16:44:16 +0200

    Add: + Css update

---
+++ Controller/requires.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+$root = $_SERVER['DOCUMENT_ROOT'];
+
+$rootHtml = "/" . basename($_SERVER['DOCUMENT_ROOT']);
+$rootHtml = str_replace("//", "/", $rootHtml);
+
+//Database
+require_once $root . "/Model/Classes/Database.php";
